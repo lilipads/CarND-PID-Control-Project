c1cfaf5738705513ddda7569c5ef6ccb964b07a5
Lili Jiang <lilijiang@Lilis-MacBook-Air.local>
2018-05-06 16:19:14 -0700

    completed project

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,85 @@
 #include "PID.h"
+#include "math.h"
 
-using namespace std;
+const double KP_STEERING = 0.18;
+const double KI = 0;
+const double KD = 2.50;
+const double KP_THROTTLE = 0.1;
+const bool TUNING_MODE = false;
+// iterations per twiddle cycle
+const int MAX_ITERATION = 2000;
+// after this iteration, we start counting error for twiddle
+const int INITIAL_ITERATION = 100;
+const int SPEED_LIMIT = 20;
 
-/*
-* TODO: Complete the PID class.
-*/
-
-PID::PID() {}
+PID::PID() {
+	Init();
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init() {
+	Kp = KP_STEERING;
+	Ki = KI;
+	Kd = KD;
+	i_error = 0.0;
+	p_error = 0.0;
+	d_error = 0.0;
+	TuningMode = TUNING_MODE;
+	if (TuningMode){
+		iteration = 0;
+		twiddle.SetParams(Kp, Ki, Kd);
+	}
 }
 
-void PID::UpdateError(double cte) {
+void PID::Reset(vector<double> params){
+	Kp = params[0];
+	Ki = params[1];
+	Kd = params[2];
+	i_error = 0.0;
+	p_error = 0.0;
+	d_error = 0.0;
+	iteration = 0;
+	twiddle.ResetError();
 }
 
-double PID::TotalError() {
+/*
+ * return true to have the main program to reset the car
+ */
+bool PID::RunTwiddle(double cte, double speed){
+	iteration++;
+	if (iteration > INITIAL_ITERATION){
+		twiddle.UpdateError(cte, speed);
+	}
+
+	if (iteration == MAX_ITERATION){
+		vector<double> params = twiddle.Run();
+		Reset(params);
+		return true;
+	}
+	return false;
 }
 
+void PID::UpdateSteeringError(double cte) {
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error += cte;
+}
+
+double PID::GetSteering(double cte) {
+	UpdateSteeringError(cte);
+	// keep it between -1 and 1
+	return -std::min(std::max(Kp * p_error + Ki * i_error + Kd * d_error, -1.0), 1.0);
+}
+
+double PID::GetThrottle(double speed) {
+	return (SPEED_LIMIT - speed) * KP_THROTTLE;
+}
+
+bool PID::TwiddleCompleted(){
+	if (twiddle.IsCompleted()){
+		std::cout << "TWIDDLE COMPLETED!" << endl;
+		std::cout << "Kp: " << Kp << " Ki: " << Ki << " Kd: " << Kd << endl;
+	}
+	return twiddle.IsCompleted();
+}
--- src/PID.h
+++ src/PID.h
@@ -1,8 +1,56 @@
 #ifndef PID_H
 #define PID_H
 
+#include "twiddle.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class PID {
 public:
+  bool TuningMode;
+
+  /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Destructor.
+  */
+  virtual ~PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init();
+
+  /*
+   * run the twiddle algorithm and re-initialize the class
+   * return true when a twiddle cycle completes and need to reset
+   */ 
+  bool RunTwiddle(double cte, double speed);
+
+  /*
+  * Return steering angle
+  */
+  double GetSteering(double cte);
+
+ /*
+  * Return throttle
+  */
+  double GetThrottle(double speed);
+
+  /*
+  * completed tuning
+  */
+  bool TwiddleCompleted();
+
+private:
+  // count number of times this controller is being called. Used in twiddle.
+  int iteration;
+
   /*
   * Errors
   */
@@ -18,29 +66,19 @@ public:
   double Kd;
 
   /*
-  * Constructor
-  */
-  PID();
-
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
-
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
+   * Use twiddle to tune parameters
+   */
+  Twiddle twiddle;
 
   /*
   * Update the PID error variables given cross track error.
   */
-  void UpdateError(double cte);
+  void UpdateSteeringError(double cte);
 
   /*
-  * Calculate the total PID error.
+  * Reset PID with given Kp, Ki, and Kd in a vector. Used for twiddle.
   */
-  double TotalError();
+  void Reset(vector<double> params);
 };
 
 #endif /* PID_H */
